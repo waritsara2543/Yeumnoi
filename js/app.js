36e8437c59b674c4ba540556c8c3b5e7ae74efbd
waritsara2543 <62234138+waritsara2543@users.noreply.github.com>
2020-11-27 11:20:27 +0700

    โครงร่าง

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDaIcVja2qOFzfpalsvfAlNVUh8usBpZIs",
+    authDomain: "yuemnouy.firebaseapp.com",
+    databaseURL: "https://yuemnouy.firebaseio.com",
+    projectId: "yuemnouy",
+    storageBucket: "yuemnouy.appspot.com",
+    messagingSenderId: "380855889580",
+    appId: "1:380855889580:web:dd628715a048c024f4f909",
+    measurementId: "G-D02XVV0RLY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
