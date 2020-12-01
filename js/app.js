37ce8a372e970e2bee0aa85461f86f047587660d
waritsara2543 <62234138+waritsara2543@users.noreply.github.com>
2020-12-01 15:09:00 +0700

    update หน้าแต่ละประเภท เสร็จแล้ว

--- js/app.js
+++ js/app.js
@@ -1,41 +1,47 @@
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDaIcVja2qOFzfpalsvfAlNVUh8usBpZIs",
-    authDomain: "yuemnouy.firebaseapp.com",
-    databaseURL: "https://yuemnouy.firebaseio.com",
-    projectId: "yuemnouy",
-    storageBucket: "yuemnouy.appspot.com",
-    messagingSenderId: "380855889580",
-    appId: "1:380855889580:web:dd628715a048c024f4f909",
-    measurementId: "G-D02XVV0RLY"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
-  
-    var db = firebase.firestore();
+  apiKey: "AIzaSyDaIcVja2qOFzfpalsvfAlNVUh8usBpZIs",
+  authDomain: "yuemnouy.firebaseapp.com",
+  databaseURL: "https://yuemnouy.firebaseio.com",
+  projectId: "yuemnouy",
+  storageBucket: "yuemnouy.appspot.com",
+  messagingSenderId: "380855889580",
+  appId: "1:380855889580:web:dd628715a048c024f4f909",
+  measurementId: "G-D02XVV0RLY"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-    
-    //หน้า แต่ละประเภท
-    document.addEventListener('init', function (event) {
-      var page = event.target;
+var db = firebase.firestore();
+
+
+//หน้า แต่ละประเภท
+document.addEventListener('init', function (event) {
+  var page = event.target;
+
+  if (page.id === 'category' || page.id === 'home') {
+    $('.category-item').off('click').on('click', function () {
       var id = $(this).attr('id');
-      if (page.id === 'category') {
-    db.collection("Products").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        var card = `<div class="card ">
-           <img class="card-img-top" src="${doc.data().Photo}" alt="">
-           <div class="card-body">
-    <h4 class="card-title"> ${doc.data().Name}</h4>
-    <p class="card-text">${doc.data().Lender} - ${doc.data().Location}</p>
-    
-  </div>
-       </div>`;
-        $("#show").append(card);
-  
+      console.log(id);
+      $("#show").empty();
+      db.collection("Products").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          if (doc.data().Category === id) {
+            var card = `<div class="card ">
+                        <img class="card-img-top" src="${doc.data().Photo}" alt="">
+                        <div class="card-body">
+                        <h4 class="card-title"> ${doc.data().Name}</h4>
+                        <p class="card-text">${doc.data().Lender} - ${doc.data().Location}</p>
+                        </div>
+                        </div>`;
+            $("#show").append(card);
+
+          }
+        });
       });
-    });
-   }
-  })
-  
-    
+    })
+  }
+})
+
+
