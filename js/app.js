d6d263311e1798ed448f150c54dc1c02e2ba37c4
waritsara2543 <62234138+waritsara2543@users.noreply.github.com>
2020-12-04 23:27:56 +0700

    update carousel,history,selectdate

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ document.addEventListener('init', function (event) {
         querySnapshot.forEach((doc) => {
           if (doc.data().Category === id) {
             var card = `
-                        <div id="${doc.data().Name}" class="detail" onclick="openPlaylist(id)">
+                        <div id="${doc.data().Name}" class="detail" onclick="showDetail(id)">
                         <div class="card " >
                         <img class="card-img-top" src="${doc.data().Photo}" alt="" >
                      
@@ -55,12 +55,34 @@ document.addEventListener('init', function (event) {
       });
     })
 
+   
+    
+    //carusel index
+    db.collection("Products").get().then((querySnapshot) => {
+      $('#carousel').empty();
+      querySnapshot.forEach((doc) => {
+        if (doc.data().Location === 'กระทู้') {
+          var card = `<ons-carousel-item modifier="nodivider" class="recomended-item" id="${doc.data().Name}"  onclick="showDetailCarousel(id)">
+          <div class="thumbnail" style="background-color: rgb(161, 114, 140);"></div>
+          <ons-list>
+              <ons-list-item>
+              <img  style="width: 100%;height: 170px;" src="${doc.data().Photo}" alt="" >
+              </ons-list-item>
+          </ons-list>
+      </ons-carousel-item>`;
+          $("#carousel").append(card);
+
+        }
+          
+      });
+  });
+
   }
 })
 
 
 //หน้า detail แต่ละสินค้า
-function openPlaylist(id) {
+function showDetail(id) {
   navcategory.pushPage('views/detail.html')
   console.log(id);
   db.collection("Products").get().then((querySnapshot) => {
@@ -94,3 +116,69 @@ function openPlaylist(id) {
     });
   });
 }
+//showdetail carousel
+function showDetailCarousel(id) {
+  navhome.pushPage('views/detail.html')
+  console.log(id);
+  db.collection("Products").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      if (doc.data().Name === id) {
+        var card = `<div class="card " >
+                        <img class="card-img-top" src="${doc.data().Photo}" alt="">
+                        <div class="container">
+                        <div class="">
+                        <h5 class="card-title"> ${doc.data().Name}</h5>
+                        <p class="card-text">${doc.data().Lender} - ${doc.data().Location}</p>
+                        <p></p>
+                         </div>
+                       
+                        <div class="col-3">
+                        <h5 class="card-text">   <font color="red">${doc.data().Price}</font></h5>
+                        <h6 class="card-text" > <font color="red">บาท </font> </h6>
+                        </div>
+                                             
+                       </div>
+
+
+                        <div class="col-12">
+                        <p class="card-text">${doc.data().Detail}</p>
+                        </div>
+                        </div>
+                        </div>`;
+        $("#showDetailList").append(card);
+
+      }
+    });
+  });
+}
+
+
+
+
+// function getmovieFavourite() {
+//   $("#showmovieFavorite").empty();
+//   db.collection("movies").get().then(function (querySnapshot) {
+//       querySnapshot.forEach(function (doc) {
+//           var user = firebase.auth().currentUser;
+//           const getUserFavorite = doc.data().uid
+//           if (getUserFavorite.indexOf(user.uid) != -1) {
+//               const result =
+//                   /*html*/
+//                   `<div class="col-6" style="padding-left:0px;padding-right:0px">
+//                       <div id="${doc.data().id}" class="imgFav d-flex align-items-end" style="background-image: url(${doc.data().posterURL}); " >
+//                           <div class="movietextbg">
+//                               <div class="movietitle-Fav">${doc.data().title}</div>
+//                           </div>
+//                       </div>
+//                   </div>`
+//               $("#showmovieFavorite").append(result)
+//           }
+//       });
+//       $("#showmovieFavorite div").click(function () {
+//           const movieTarget = $(this).attr('id');
+//           console.log(movieTarget);
+//           getmovieDetail(movieTarget);
+//           document.querySelector("#Navigator_favorite").pushPage("views/movieDetail.html");
+//       })
+//   });
+// }
--- js/index.js
+++ js/index.js
@@ -1,43 +1,74 @@
 window.fn = {};
 
-window.fn.open = function() {
+window.fn.open = function () {
   var menu = document.getElementById('menu');
-  
+
   menu.open();
 };
 
-window.fn.load = function(page) {
+window.fn.load = function (page) {
   var content = document.getElementById('content');
   var menu = document.getElementById('menu');
-  
+
   content.load(page)
     .then(menu.close.bind(menu));
-   
+
 };
 
 
 //alert เลือกวันที่ยืม
-var selectDate = function() {
+var selectDate = function () {
   var dialog = document.getElementById('my-alert-dialog');
 
   if (dialog) {
     dialog.show();
   } else {
     ons.createElement('alert-dialog.html', { append: true })
-      .then(function(dialog) {
+      .then(function (dialog) {
         dialog.show();
       });
   }
+
 };
-var hideAlertDialog = function() {
+var OKAlertDialog = function () {
+
+  var daysDiff;
+
+
+  var startDate = document.getElementById('startDate').value;
+  var endDate = document.getElementById('endDate').value;
+  console.log(startDate);
+  console.log(endDate);
+  startDate = startDate.split("-");
+  endDate = endDate.split("-");
+  sDate = new Date(startDate[0], startDate[1] - 1, startDate[2]);
+  eDate = new Date(endDate[0], endDate[1] - 1, endDate[2]);
+  daysDiff = Math.round((eDate - sDate) / 86400000);
+  console.log(daysDiff);
+  document.getElementById("incorrectDate").innerHTML = "*****กรุณากรอกวันที่ให้ถูกต้อง*****";
+  if (daysDiff > 0) {
+    
+    document.getElementById('my-alert-dialog')
+      .hide();
+  }
+
+
+};
+var cancleAlertDialog = function () {
+
+
   document
     .getElementById('my-alert-dialog')
     .hide();
-};
+}
 
-var notify = function() {
+
+
+
+var notify = function () {
   ons.notification.alert('This dialog was created with ons.notification');
 };
+
 
 
 
@@ -90,8 +121,8 @@ var notify = function() {
 
 
 
-            
-    
+
+
 
 
 
